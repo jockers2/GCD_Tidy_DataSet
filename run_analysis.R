08745fca6c659cf50233d450a0ccf6a992862319
Jim Ockers <jim.ockers@gmail.com>
2014-07-24 22:17:17 -0400

    Added code to read training data

--- run_analysis.R
+++ run_analysis.R
@@ -22,3 +22,36 @@ if (!file.exists(localFilename)) {
     print("Using local copy of data set")
 }
 
+dataFileList <- unzip(localFilename,list = TRUE)
+
+if (!file.exists("data")) {  
+  unzip(localFilename, overwrite=FALSE, exdir="data")
+}
+
+## start reading data
+
+activity_labels <- read.table("./data/UCI HAR Dataset/activity_labels.txt", sep = " ", header=FALSE)
+features <- read.table("./data/UCI HAR Dataset/features.txt", sep = " ", header=FALSE)
+
+colnames(activity_labels) <- c("ID","Activity")
+
+## read training data set
+
+subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt", sep = " ", header=FALSE)
+if (!exists("X_train_data")) {
+  X_train_data <- read.table("./data/UCI HAR Dataset/train/X_train.txt", header=FALSE)
+}
+y_train_labels <- read.table("./data/UCI HAR Dataset/train/y_train.txt", header=FALSE)
+
+colnames(X_train_data) <- features[,2]
+colnames(subject_train) <- "Subject"
+colnames(y_train_labels) <- c("Activity")
+foobar <- factor(y_train_labels, levels=activity_labels$ID, labels=activity_labels$Activity)
+
+## find indices of columns with mean or standard deviation measurements
+
+train_means <- grep("mean()",features[,2])
+train_stds <- grep("std()",features[,2])
+train_indices <- sort(c(train_means,train_stds))
+
+
